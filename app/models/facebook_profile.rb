53e2a4e41f44528ecb0d599e5dd6c7f96d4b3749
Grigoriy Dobryakov <grigoriydobryakov@gmail.com>
2015-03-14 20:21:43 +0100

    add facebook profile

---
+++ app/models/facebook_profile.rb
@@ -0,0 +1,3 @@
+class FacebookProfile < Profile
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314191734) do
+ActiveRecord::Schema.define(version: 20150314191945) do
 
   create_table "profiles", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
     t.string   "type",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "token",      limit: 255
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
