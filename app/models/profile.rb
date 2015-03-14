65f35c94f94471103db369f22ff8738a10e72329
Grigoriy Dobryakov <grigoriy@dobryakov.com>
2015-03-15 01:19:09 +0300

    update profiles (create user)

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,8 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   has_many   :messages
 
+  after_create do
+    self.update( user: User.create ) if !self.user
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
 
   has_many :profiles
 
+  def name
+    self.profiles.first.name if self.profiles.first
+  end
+
 end
